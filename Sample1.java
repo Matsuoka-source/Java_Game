925ec0ab94fa226930cea323cdbec9a690da1d7c
Matsuoka-source <tm19930425@gmail.com>
2021-08-19 15:49:09 +0900

    add

--- Sample1.java
+++ Sample1.java
@@ -1,9 +1,19 @@
-class Sample1 {
+public class Sample1 {
   public static void main(String[] args) {
-    System.out.println("こんにちはJAVA!");
+
+    // 登場人物
+    String k = "王";
+    String name = "アーサー" + k;
+
+    // レベル
+    int lv = 50;
+
+    // 文章
+    System.out.println("魔王が正解を滅ぼそうとしています。");
+    System.out.println(name + "はレベル" + lv + "の強者です。");
+    System.out.println(name + "は魔王を倒しました。");
+    System.out.println("Game CLEAR!!");
   }
 }
-
-
 
 // vscodeだとコンパイルをしなくても右上の再生スタートみたいなやつで表示できる。
