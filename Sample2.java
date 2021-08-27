892c5dc46dbd426f95e4102c39ccb22d3d38784a
Matsuoka-source <tm19930425@gmail.com>
2021-08-27 15:11:11 +0900

    add

--- Sample2.java
+++ Sample2.java
@@ -1,18 +1,32 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.awt.Font;
+import java.awt.Color;
+
 public class Sample2 {
   public static void main(String[] args) {
     System.out.println("Hello world!!");
 
-    javax.swing.JFrame jf = new javax.swing.JFrame();
+    JFrame jf = new JFrame();
     jf.setBounds(50, 100, 750, 400);// 最初の二つが表示したい場所 windowの幅
     jf.setVisible(true);// windowを表示する。
-    jf.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);// windowを閉じるとプログラムも終了する
+    jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// windowを閉じるとプログラムも終了する
 
     // 文字の表示
-    javax.swing.JLabel jl = new javax.swing.JLabel("Hello world!!");
-    jl.setFont(new java.awt.Font(java.awt.Font.SERIF, java.awt.Font.ITALIC, 30));// 文字のサイズ変更 name,style,size 文字の形を変更できる
-    jl.setForeground(new java.awt.Color(255, 128, 0));
+
+    JPanel jp = new JPanel();
+    jp.setBackground(Color.BLACK);
+    jf.add(jp);
+
+    JLabel jl = new JLabel("Hello world!!");
+    Font f = new Font(Font.SERIF, Font.ITALIC, 30);
+    jl.setFont(f);// 文字のサイズ変更 name,style,size 文字の形を変更できる
+    jl.setForeground(new Color(255, 128, 0));
     // 文字の色を変更java.awt.Color.RED newを付けてColorの後に()内に3つ数字を入れることで自分の好きな色に変更することができる。
-    jf.add(jl);
+    jp.add(jl);
+    // jlとjpの順番が反対になるとどちらかが下に行って見えなくなることあり
   }
 }
 
@@ -21,3 +35,6 @@ public class Sample2 {
 // windowを消してもプログラムが終了しない
 // Swing
 // Jラベルによる文字列の表示
+// 先頭にimport javax.swingと記述するとmainの中でjavax.swingを省略できる
+// javax.swing.*;だと広範囲だけど後でどこに使用されているのかわかりにくい
+// そのため*を消してそのあとのJFrameやJLabelを記述して、元あった場所はjavax.swingの省略可能
