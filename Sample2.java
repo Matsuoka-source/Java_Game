cd65d96b5d2bee9f5e9cc6a98e18e5605b14c729
Matsuoka-source <tm19930425@gmail.com>
2021-08-27 13:51:37 +0900

    add

--- Sample2.java
+++ Sample2.java
@@ -1,13 +1,21 @@
 public class Sample2 {
   public static void main(String[] args) {
-    System.out.println("hello world!!");
+    System.out.println("Hello world!!");
+
     javax.swing.JFrame jf = new javax.swing.JFrame();
-    jf.setBounds(20, 50, 300, 200);// 最初の二つが表示したい場所 windowの幅
+    jf.setBounds(50, 100, 750, 400);// 最初の二つが表示したい場所 windowの幅
     jf.setVisible(true);// windowを表示する。
     jf.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);// windowを閉じるとプログラムも終了する
+    
+    // 文字の表示
+    javax.swing.JLabel jl = new javax.swing.JLabel("Hello world!!");
+    jl.setFont(new java.awt.Font(null,0,30));//文字のサイズ変更
+    jf.add(jl);
   }
 }
 
 // windowサイズが小さい
 // 表示場所
 // windowを消してもプログラムが終了しない
+// Swing
+// Jラベルによる文字列の表示
