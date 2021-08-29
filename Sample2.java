e48e4488bf0163d0667906b3d5fed63fda9b596e
Matsuoka-source <tm19930425@gmail.com>
2021-08-30 08:50:15 +0900

    add

--- Sample2.java
+++ Sample2.java
@@ -9,6 +9,7 @@ public class Sample2 {
   public static void main(String[] args) {
     System.out.println("Hello world!!");
 
+    // ウィンドウフレームを作成、表示
     JFrame jf = new JFrame();
     jf.setBounds(50, 100, 750, 400);// 最初の二つが表示したい場所 windowの幅
     jf.setVisible(true);// windowを表示する。
@@ -16,10 +17,12 @@ public class Sample2 {
 
     // 文字の表示
 
+    // 親パネルを作成し、親パネルに張り付け
     JPanel jp = new JPanel();
     jp.setBackground(Color.BLACK);
     jf.add(jp);
 
+    // ラベルを作成し、親パネルに張り付け
     JLabel jl = new JLabel("Hello world!!");
     Font f = new Font(Font.SERIF, Font.ITALIC, 30);
     jl.setFont(f);// 文字のサイズ変更 name,style,size 文字の形を変更できる
@@ -27,6 +30,9 @@ public class Sample2 {
     // 文字の色を変更java.awt.Color.RED newを付けてColorの後に()内に3つ数字を入れることで自分の好きな色に変更することができる。
     jp.add(jl);
     // jlとjpの順番が反対になるとどちらかが下に行って見えなくなることあり
+    JPanel jpChild = new JPanel();
+    jpChild.setBackground(Color.RED);
+    jp.add(jpChild);
   }
 }
 
