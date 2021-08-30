a85c091eff1178869a57bd6b3fcc3eae4f5b40dd
Matsuoka-source <tm19930425@gmail.com>
2021-08-30 09:10:04 +0900

    add

--- Sample2.java
+++ Sample2.java
@@ -30,9 +30,19 @@ public class Sample2 {
     // 文字の色を変更java.awt.Color.RED newを付けてColorの後に()内に3つ数字を入れることで自分の好きな色に変更することができる。
     jp.add(jl);
     // jlとjpの順番が反対になるとどちらかが下に行って見えなくなることあり
+
+    // パネルを作成し、親パネルに貼り付け
     JPanel jpChild = new JPanel();
     jpChild.setBackground(Color.RED);
     jp.add(jpChild);
+
+    // ラベルを作成し、子パネルに張り付け
+    JLabel jlChild = new JLabel("Hello");
+    jlChild.setFont(f);// 文字のサイズ変更 name,style,size 文字の形を変更できる
+    jlChild.setForeground(Color.WHITE);
+    // 文字の色を変更java.awt.Color.RED newを付けてColorの後に()内に3つ数字を入れることで自分の好きな色に変更することができる。
+    jpChild.add(jlChild);
+
   }
 }
 
@@ -44,3 +54,6 @@ public class Sample2 {
 // 先頭にimport javax.swingと記述するとmainの中でjavax.swingを省略できる
 // javax.swing.*;だと広範囲だけど後でどこに使用されているのかわかりにくい
 // そのため*を消してそのあとのJFrameやJLabelを記述して、元あった場所はjavax.swingの省略可能
+
+// 8/30
+// 部品の重ね合わせ
